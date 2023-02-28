4b90ea3f271dd9defa36622819076e6626711c2e
ruko19 <rukobaruna23@gmail.com>
2023-02-28 17:18:06 -0500

    se añade title & target component

---
+++ src/layouts/RecycleTarget/RecycleTarget.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const RecycleTarget = () => {
+
+    const infoTarget = [
+        {
+            "icon": "",
+            "title": "hola bb",
+            "description": "asdfasdfasdfasdfasdfñlkajsdlkfjalksdjfñlaksdjfñlkajsñdfkl"
+        },
+        {
+            "icon": "",
+            "title": "hola bb",
+            "description": "asdfasdfasdfasdfasdfñlkajsdlkfjalksdjfñlaksdjfñlkajsñdfkl"
+        },
+        {
+            "icon": "",
+            "title": "hola bb",
+            "description": "asdfasdfasdfasdfasdfñlkajsdlkfjalksdjfñlaksdjfñlkajsñdfkl"
+        },
+        {
+            "icon": "",
+            "title": "hola bb",
+            "description": "asdfasdfasdfasdfasdfñlkajsdlkfjalksdjfñlaksdjfñlkajsñdfkl"
+        }
+    ]
+
+
+
+
+
+    return (
+        <div>hola</div>
+    )
+}
+
+export default RecycleTarget
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
+import RecycleTarget from "../layouts/RecycleTarget/RecycleTarget"
+
 const Home = () => {
     return (
         <>
-            <h1>hola home</h1>
+            <div className="w-full text-center">
+                <div className="max-w-screen-lg m-auto">
+                    <h2 className="text-7xl font-bold mb-11">¿QUE HACEMOS <br />Y COMO LO <span className="text-lime-500">HACEMOS ?</span></h2>
+                    <p className="leading-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quasi optio iusto tempore minima quam expedita? Autem molestiae architecto quis adipisci est! Repellendus, fugit accusamus. Alias, eaque unde? Laudantium, voluptas.</p>
+                </div>
+                <section className="w-full  bg-lime-500 grid grid-cols-2">
+                    <RecycleTarget />
+                    <RecycleTarget />
+                    <RecycleTarget />
+                    <RecycleTarget />
+
+                </section>
+            </div>
         </>
     )
 }
