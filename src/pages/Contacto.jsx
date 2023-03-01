5b26d187f813b27058805f054fd92f8a8e50fedc
miloa2002 <agudelo.tapias@gmail.com>
2023-02-28 22:46:54 -0500

    se agrega la página de contacto

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,8 +1,12 @@
+import FormularioContacto from "../components/Contacto/FormularioContacto"
+import ImgContacto from "../components/Contacto/ImgContacto"
+
 
 const Contacto = () => {
     return (
-        <div>
-            Hola contacto
+        <div className="container flex items-center justify-between gap-8 text-center">
+            <FormularioContacto />
+            <ImgContacto />
         </div>
     )
 }
