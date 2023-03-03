f6e26a23d9aa3438b0e83e29ad27b5ca1e2f05e8
ruko19 <rukobaruna23@gmail.com>
2023-03-03 08:58:41 -0500

    add eventos provider

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -8,24 +8,28 @@ import Contacto from "../pages/Contacto"
 import LoginAdmin from "../pages/LoginAdmin"
 import { EventosProvider } from "../context/EventosContext"
 
+
 // import redux toolkit
 // import { store } from "../app/store"
 // import { Provider } from 'react-redux'
 
 function App() {
   return (
+    <EventosProvider>
 
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/quienes-somos" element={<QuienesSomos />} />
-          <Route path="/eventos" element={<Eventos />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/Contacto" element={<Contacto />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/quienes-somos" element={<QuienesSomos />} />
+            <Route path="/eventos" element={<Eventos />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/Contacto" element={<Contacto />} />
+            <Route path="/login-admin" element={<LoginAdmin />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </EventosProvider>
 
 
   )
