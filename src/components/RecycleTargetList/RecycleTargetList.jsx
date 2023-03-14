75cd67c02e2984e90be1fcb03fce971c25886674
miloa2002 <agudelo.tapias@gmail.com>
2023-03-14 17:12:40 -0500

    agregado iconos en pagina home

--- src/components/RecycleTargetList/RecycleTargetList.jsx
+++ src/components/RecycleTargetList/RecycleTargetList.jsx
@@ -6,28 +6,28 @@ const RecycleTargetList = () => {
   const infoTarget = [
     {
       id: "1",
-      icon: "../../../public/icono.png",
+      icon: "https://res.cloudinary.com/dwvchf8al/image/upload/v1678831561/icono_x3wrku.png",
       title: "RECICLAR",
       description:
         "Reciclar es más que una acción, es el valor de la responsabilidad por preservar los recursos naturales.",
     },
     {
       id: "2",
-      icon: "../../../public/icono.png",
+      icon: ".https://res.cloudinary.com/dwvchf8al/image/upload/v1678831561/icono_x3wrku.png",
       title: "REUTILIZAR",
       description:
         "Utiliza la creatividad y dales otro uso a los objetos que parecen no tenerlo.",
     },
     {
       id: "3",
-      icon: "../../../public/icono.png",
+      icon: "https://res.cloudinary.com/dwvchf8al/image/upload/v1678831561/icono_x3wrku.png",
       title: "SER UN CONSUMIDOR RESPONSABLE",
       description:
         "Consumir de forma responsable es más que un término que está de moda, es una forma de entender la vida.",
     },
     {
       id: "4",
-      icon: "../../../public/icono.png",
+      icon: "https://res.cloudinary.com/dwvchf8al/image/upload/v1678831561/icono_x3wrku.png",
       title: "REDUCCIÓN",
       description:
         "Toma conciencia, reduce tu consumo, piensa en los demás.",
