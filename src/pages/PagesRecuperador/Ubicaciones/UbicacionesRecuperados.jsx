2ec16969b69ccdc958ab6a3b92d5dc4630958f68
ruko19 <rukobaruna23@gmail.com>
2023-03-22 11:27:45 -0500

    finish3

--- src/pages/PagesRecuperador/Ubicaciones/UbicacionesRecuperados.jsx
+++ src/pages/PagesRecuperador/Ubicaciones/UbicacionesRecuperados.jsx
@@ -4,6 +4,7 @@ import MapView from '../../../components/MapView/MapView'
 const UbicacionesRecuperados = () => {
     return (
         <div className='w-full mt-12 md:mt-0 z-0'>
+            <h1 className='ml-10 mb-10 text-6xl'>Ubicaciones</h1>
             <MapView />
 
         </div>
