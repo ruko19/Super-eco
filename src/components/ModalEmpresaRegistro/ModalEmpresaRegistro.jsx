058b63a1d6617bcb252fc8431f176bd5a6c6b9a7
ruko19 <rukobaruna23@gmail.com>
2023-03-22 11:11:23 -0500

    finisxh

--- src/components/ModalEmpresaRegistro/ModalEmpresaRegistro.jsx
+++ src/components/ModalEmpresaRegistro/ModalEmpresaRegistro.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import { FaRegTimesCircle } from "react-icons/fa";
 import { useAlianzas } from "../../hooks/useAlianzas"
@@ -57,10 +56,10 @@ const ModalEmpresaRegistro = ({ setModalAlianza }) => {
 
 
     return (
-        <div className="bg-black bg-opacity-90 h-screen w-full absolute top-0 bottom-0 left-0 right-0">
+        <div className=" bg-black bg-opacity-90 h-screen w-full absolute top-0 bottom-0 left-0 right-0">
 
             <FaRegTimesCircle
-                className="text-white text-4xl m-8 cursor-pointer"
+                className="text-white text-4xl m-4 cursor-pointer"
                 onClick={handleBorrarModalEmpresa}
             />
             <div className="w-1/2 mx-auto">
@@ -68,17 +67,17 @@ const ModalEmpresaRegistro = ({ setModalAlianza }) => {
 
                 <form onSubmit={handleSubmit}>
 
-                    <input value={name} onChange={e => setName(e.target.value)} className=' mt-9 mb-2 p-3 rounded-2xl outline-none w-full' type="text" placeholder='Nombre empresa' />
-                    <input value={rut} onChange={e => setRut(e.target.value)} className=' mt-9 mb-2 p-3 rounded-2xl outline-none w-full' type="number" placeholder='RUT de la empresa' />
-                    <input value={correo} onChange={e => setCorreo(e.target.value)} className=' mt-9 mb-2 p-3 rounded-2xl outline-none w-full' type="email" placeholder='Correo' />
-                    <input value={contacto} onChange={e => setContacto(e.target.value)} className=' mt-9 mb-2 p-3 rounded-2xl outline-none w-full' type="number" placeholder='Número de contacto' />
-                    <input value={direccion} onChange={e => setdireccion(e.target.value)} className=' mt-9 mb-2 p-3 rounded-2xl outline-none w-full' type="string" placeholder='Direccion' />
+                    <input value={name} onChange={e => setName(e.target.value)} className=' mt-9 mb-2 p-2 rounded-2xl outline-none w-full' type="text" placeholder='Nombre empresa' />
+                    <input value={rut} onChange={e => setRut(e.target.value)} className=' mt-9 mb-2 p-2 rounded-2xl outline-none w-full' type="number" placeholder='RUT de la empresa' />
+                    <input value={correo} onChange={e => setCorreo(e.target.value)} className=' mt-9 mb-2 p-2 rounded-2xl outline-none w-full' type="email" placeholder='Correo' />
+                    <input value={contacto} onChange={e => setContacto(e.target.value)} className=' mt-9 mb-2 p-2 rounded-2xl outline-none w-full' type="number" placeholder='Número de contacto' />
+                    <input value={direccion} onChange={e => setdireccion(e.target.value)} className=' mt-9 mb-2 p-2 rounded-2xl outline-none w-full' type="string" placeholder='Direccion' />
 
                     <div className="mt-5">
                         <label htmlFor="horarios" className="text-white block mb-2">Horarios en las que el recuperador va por los residuos:</label>
                         <div className="md:flex gap-4">
-                            <input value={horario1} onChange={e => sethorario1(e.target.value)} className='mb-2 p-3 rounded-2xl outline-none w-1/2' type="text" placeholder='Horario 1' />
-                            <input value={horario2} onChange={e => sethorario2(e.target.value)} className='mb-2 p-3 rounded-2xl outline-none w-1/2' type="text" placeholder='Horario 2' />
+                            <input value={horario1} onChange={e => sethorario1(e.target.value)} className='mb-2 p-2 rounded-2xl outline-none w-1/2' type="text" placeholder='Horario 1' />
+                            <input value={horario2} onChange={e => sethorario2(e.target.value)} className='mb-2 p-2 rounded-2xl outline-none w-1/2' type="text" placeholder='Horario 2' />
                         </div>
                     </div>
 
